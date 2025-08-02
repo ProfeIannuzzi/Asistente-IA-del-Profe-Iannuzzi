2914dc894a3575caf2f4aedd97c9ea5e57041d16
Profe Iannuzzi <profe.iannuzzi@gmail.com>
2025-08-01 23:22:57 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+async function ask() {
+  const password = document.getElementById('password').value;
+  if (password !== 'mfi') {
+    alert("No, No, No....  ESA NO ES!!");
+    return;
+  }
+
+  const question = document.getElementById('question').value;
+
+  const response = await fetch("https://asistente-ia-del-profe-iannuzzi-2.onrender.com/api/ask", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ question })
+  });
+
+  const data = await response.json();
+
+  if (data?.answer) {
+    document.getElementById("answer").innerText = data.answer;
+  } else {
+    document.getElementById("answer").innerText = "Error al generar respuesta.";
+  }
+}
